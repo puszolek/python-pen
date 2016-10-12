b7c86385dc78955f1889a72d95855f49d39faabc
Paula Kochanska <puszolek@users.noreply.github.com>
2016-10-12 13:05:57 +0100

    Update tcp_c.py

--- tcp_c.py
+++ tcp_c.py
@@ -3,13 +3,13 @@ import socket
 target_host = 'www.google.pl'
 target_port = 80
 
-#utworzenie obiektu gniazda
+#creating socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#polaczenie sie z klientem
+#connecting to client
 client.connect((target_host,target_port))
 
-#wysylanie danych
+#sending data
 client.send('GET / HTTP/1.1\r\nHost: google.pl\r\n\r\n')
 
 response = client.recv(4096)
